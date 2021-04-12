10543cd2f86c9daaf7ee2f4b5c80789fc9f4fe72
MauroBruno1990 <maurobruno_@hotmail.com>
2021-04-12 14:59:23 -0300

    atlas

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./config/db");
 // const db = require('./config/db');
 const app = express();
+const cors = require('cors')
 
 const morgan = require("morgan");
 const routes = require("./routes");
@@ -10,6 +11,8 @@ app.use(morgan("tiny"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors())
 
 app.use("/api", routes);
 
