3770e5fd3f87c3e77a7174c27d223343da88be97
MauroBruno1990 <maurobruno_@hotmail.com>
2021-04-12 15:34:27 -0300

    cors

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ const cors = require('cors');
 require("./config/db");
 
 const app = express();
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.use(cors());
 
 const morgan = require("morgan");
 const routes = require("./routes");
