53af56829758fd7d58e4a750c623992978ccb3d8
Diego <diego-jofre@live.com.ar>
2021-04-15 20:58:32 -0300

    controlador correo donacion

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,11 @@ const express = require("express")
 const router = express.Router()
 
 const userRouter = require("./userRouter")
-
 const sendMailController = require("../controllers/sendMailController")
+const sendMailDonationController = require("../controllers/sendMailDonationController")
 
 router.use("/user", userRouter)
 router.post("/send-mail", sendMailController.send)
-
-
+router.post("/send-mail-donation", sendMailDonationController.send)
 
 module.exports = router
