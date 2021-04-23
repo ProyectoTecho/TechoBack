04a9cf3e66cd10c9fca49c9c8936c00c0c81c6c8
Jesus <jesusjpelozo@gmail.com>
2021-04-23 10:06:01 -0300

    cambios en el mail del footer

--- controllers/sendMailController.js
+++ controllers/sendMailController.js
@@ -11,12 +11,19 @@ const transporter = nodemailer.createTransport({
 const sendMailController = {
   send(req, res) {
     console.log(req.body)
-    const { nombre, email } = req.body;
-    contentHTML = `
+    const { nombre, email, telefono, infoContacto } = req.body;
+    contentHTMLClient = `
          <p>${nombre}, este correo es para confirmar que hemos recibido el formulario completo.
          Estaremos confirmando los datos ingresados para que puedas formar parte de la Comunidad de Socios/as que hacen posible nuestra acción en más de 80 barrios populares y nos volveremos a comunicar pronto.
          ¡Gracias por estar del otro lado!</p> 
       `;
+      contentHTMLTecho = `
+      <p> Nombre: ${nombre}, <br/>
+      Email: ${email}, <br/>
+      Telefono: ${telefono}, <br/>
+      Informacion: ${infoContacto}, <br/>
+      </p> 
+   `;
 
     transporter.sendMail(
       {
@@ -25,6 +32,14 @@ const sendMailController = {
         subject: "¡Gracias por querer sumarte!",
         html: contentHTML,
       },
+
+      {
+        from: "proyecto.techo.ong@gmail.com",
+        to: "proyecto.techo.ong@gmail.com",
+        subject: "Nueva consulta",
+        html: contentHTMLTecho,
+      },
+
       function (error, info) {
         if (error) {
           console.log(error);
